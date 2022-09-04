12e7078712ba31825366374f2e24e9031da45f11
bablyaisha <bablyaisha786@gmail.com>
2022-09-04 12:20:22 +0530

    MathDSA #1

--- src/Bit_ith.java
+++ src/Bit_ith.java
@@ -1,16 +1,19 @@
 public class Bit_ith {
     public static void main(String[] args) {
-       int n=118;
+       int n=97;
         int i=2;
         System.out.println("without change "+n+" is "+ Integer.toBinaryString(n));
 //        System.out.println("Numeric rep: "+ithBit(n,i));
 
 //        System.out.println("Numeric rep in set Bit: "+setBit(n,i));
-        System.out.println("Numeric rep in set Bit: "+reset(n,i));
+//        System.out.println("Numeric rep in set Bit: "+reset(n,i));
+        System.out.println(position_right(n));
         //Binary representation
 //        System.out.println("Bianry rep: "+Integer.toBinaryString(ithBit(n,i)));
 //        System.out.println("Bianry rep in set Bit: "+Integer.toBinaryString(setBit(n,i)));
-        System.out.println("Bianry rep in set Bit: "+Integer.toBinaryString(reset(n,i)));
+//        System.out.println("Bianry rep in set Bit: "+Integer.toBinaryString(reset(n,i)));
+
+
     }
     //find the ith Bit.
     static int ithBit(int n,int i){
@@ -27,6 +30,8 @@ public class Bit_ith {
        return n& ~(1<<(i-1));   //mask is compliment of 1<<(i-1) and reset means 0->0 and 1->0
     }
     //find the position of right most set bit
-
+     static int position_right(int n){
+        return n& -n;
+     }
 
 }
