62ba070fe970d4f346fbd1be1f76f74ac59245c7
bablyaisha <bablyaisha786@gmail.com>
2022-09-06 00:43:04 +0530

    MathDSA #2

---
+++ src/TotalSetBits.java
@@ -0,0 +1,22 @@
+public class TotalSetBits {
+    public static void main(String[] args) {
+        int n=254;
+        System.out.println(Integer.toBinaryString(n));
+        System.out.println("Number of set bits are: "+setBits(n));
+    }
+
+    static int setBits(int n){
+        //M-1
+        int count=0;
+//        while (n>0){
+//            count++;
+//            n=n-(n&(-n));
+//        }
+        //M-2
+        while (n>0){
+            count++;
+            n=n&(n-1);
+        }
+        return count;
+    }
+}
