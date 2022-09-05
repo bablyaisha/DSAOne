469c12ea70d6b8fbf6dc28f3209a138fa947ffa3
bablyaisha <bablyaisha786@gmail.com>
2022-09-05 23:21:39 +0530

    MathDSA #1

---
+++ src/PascalsTriangle.java
@@ -0,0 +1,22 @@
+public class PascalsTriangle {
+    public static void main(String[] args) {
+         int n=10;
+        System.out.println(row_sum(n));
+
+    }
+    static int row_sum(int n){
+        //n is the row whose sum is required
+        //M-1 T(N)= o(N)
+        int sum=0;
+//        for (int i = 0; i <n ; i++) {
+//             sum+=(1<<i);
+//        }
+//        return sum;
+//
+        //M-2 T(N)=o(1)
+        sum = 1<<n;
+        return sum-1;
+
+
+    }
+}
